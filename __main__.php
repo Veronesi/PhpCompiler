10637ca1a2ca93d002459a5ff262db2152595357
Veronesi <veronesif@frcu.utn.edu.ar>
2019-05-12 17:30:51 -0300

    agregue mas comandos

--- __main__.php
+++ __main__.php
@@ -1,14 +1,28 @@
 <?php
+    include('Arbol.php');
+    include('Producciones.php');
+    include('Terminales.php');
+    include('Variables.php');
+    include_once('Color.php');
+
+    use \PhpCompiler\Producciones;
+    use \PhpCompiler\Terminales;
+    use \PhpCompiler\Variables;
+    use \PhpCompiler\Arbol;
+    use \PhpCompiler\Color;
+
 $command = array(
     '-al <file>'    => " Realiza un analisis lexico",
     '-as <file>'    => " Realiza un analisis sintactico",
-    '-c  <file>'     => " Compliar archivo",
+    '-c  <file>'    => " Compliar archivo",
     '-d'            => " Modo debug (muestra paso a paso la compilacion)",
+    '-f'            => " Fuerza a compilar un archivo",
     '-h'            => " Muestra una ayuda",
     '-p'            => " Muestra la lista de las producciones",
-    '-r  <file>'     => " Ejecuta un archivo compilado",
+    '-r  <file>'    => " Ejecuta un archivo compilado",
     '-t'            => " Muestra la lista de terminales",
     '-v'            => " Muestra la lista de las variables",
+    'foo="bar"'     => " Pasa como parametro una variable"
 
 );
 
@@ -17,6 +31,17 @@ if(in_array("-h", $_SERVER['argv'])){
         print str_pad("\n   ".$keyC, 20).$value; 
     }
     print "\n\n   Para mas ayuda https://github.com/Veronesi/PhpCompiler\n";
+}elseif(in_array("-t", $_SERVER['argv'])){
+    foreach (\PhpCompiler\Terminales\terminales as $keyT => $value) {
+        print "\n   ".$value; 
+    }
+    print "\n"; 
+}elseif (in_array("-p", $_SERVER['argv'])) {
+    foreach (\PhpCompiler\Producciones\producciones as $keyP => $value) {
+        $arbol = new Arbol(key(\PhpCompiler\Producciones\producciones[$keyP]), $value[key($value)]);
+        $arbol->MostrarArbol(); 
+        print "\n\n"; 
+    }    
 }
 
 /*
